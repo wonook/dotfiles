b8b71cf5e3b571a4682b84e0e71f5ac4109abca9
Won Wook SONG <wsong0512@gmail.com>
2015-02-14 17:52:39 +0900

    added shell script

---
+++ make.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+
+read -r -p "Replace the current bash settings? [Y/n]" response
+case $response in
+  [Yy]* )
+    cp .bash_profile ~/
+    cp .bashrc ~/
+    cp .git-completion.bash ~/
+    ;;
+  * ) 
+    echo "bash settings are not copied\n"
+    ;;
+esac
+
+read -r -p "Replace current irb settings? [Y/n]" response
+case $response in
+  [Yy]* )
+    cp .irbrc ~/
+    cp .gemrc ~/
+    ;;
+  * ) 
+    echo "irb settings are not copied\n"
+    ;;
+esac
+
+read -r -p "Replace current vim settings and plugins? [Y/n]" response
+case $response in
+  [Yy]* )
+    git submodule init
+    git submodule update
+    rm -rf ~/.vimrc
+    rm -rf ~/.vim/
+    cp -R .vim ~/
+    cp -R .vimrc ~/
+    ;;
+  * ) 
+    echo "vim settings are not copied\n"
+    ;;
+esac
+
