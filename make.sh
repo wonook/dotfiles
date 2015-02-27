ae44c1707f9a9daa033e613d90791fb9846b4001
Won Wook SONG <wsong0512@gmail.com>
2015-02-28 00:58:08 +0900

    included rbenv installation

--- make.sh
+++ make.sh
@@ -17,6 +17,26 @@ case $response in
     ;;
 esac
 
+read -r -p "Install rbenv? [Y/n]" response
+case $response in
+  [Yy]* )
+    rm -rf ~/.rbenv
+    git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+    echo "installed into .rbenv"
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+
+    git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+    echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+
+    git clone git://github.com/rkh/rbenv-whatis.git ~/.rbenv/plugins/rbenv-whatis
+    git clone git://github.com/rkh/rbenv-use.git ~/.rbenv/plugins/rbenv-use
+
+    ;;
+  * ) 
+    echo "rbenv is not installed\n"
+    ;;
+esac
+
 read -r -p "Replace current irb settings? [Y/n]" response
 case $response in
   [Yy]* )
