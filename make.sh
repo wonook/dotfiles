f60bac4d737eb124a0d1fbd0bc335459c228e345
Won Wook SONG <wsong0512@gmail.com>
2015-02-28 02:33:39 +0900

    added rbenv install for brew

--- make.sh
+++ make.sh
@@ -20,24 +20,45 @@ esac
 read -r -p "Install rbenv? [Y/n]" response
 case $response in
   [Yy]* )
-    rm -rf ~/.rbenv
-    git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
-    echo "installed into .rbenv"
+    read -r -p "Are you on mac? [Y/n]" resp
+    case $resp in
+      [Yy]* )
+        brew install rbenv
+        echo "brew installed rbenv"
+        brew install ruby-build
+        echo "brew installed ruby-build"
+        brew install rbenv-whatis
+        echo "brew installed rbenv-whatis"
+        brew install rbenv-use
+        echo "brew installed rbenv-use"
+        brew install rbenv-gem-rehash
+        echo "brew installed rbenv-gem-rehash"
+        brew install rbenv-default-gems
+        echo "bundler\nrails\ncapistrano\npassenger" > "$RBENV_ROOT/default-gems"
+        echo "brew installed rbenv-default-gems"
+        brew install rbenv-gemset
+        echo "brew installed rbenv-gemset"
+        ;;
+      * )
+        rm -rf ~/.rbenv
+        git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+        echo "installed into .rbenv"
 
-    git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-    echo "installed ruby-build"
-    git clone git://github.com/rkh/rbenv-whatis.git ~/.rbenv/plugins/rbenv-whatis
-    echo "installed rbenv-whatis"
-    git clone git://github.com/rkh/rbenv-use.git ~/.rbenv/plugins/rbenv-use
-    echo "installed rbenv-use"
-    git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
-    echo "installed rbenv-gem-rehash"
-    git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
-    echo "installed rbenv-default-gems"
-    echo "bundler\nrails\ncapistrano\npassenger" > ~/.rbenv/default-gems
-    git clone git://github.com/jf/rbenv-gemset.git ~/.rbenv/plugins/rbenv-gemset
-    echo "installed rbenv-gemset"
-    ;;
+        git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+        echo "installed ruby-build"
+        git clone git://github.com/rkh/rbenv-whatis.git ~/.rbenv/plugins/rbenv-whatis
+        echo "installed rbenv-whatis"
+        git clone git://github.com/rkh/rbenv-use.git ~/.rbenv/plugins/rbenv-use
+        echo "installed rbenv-use"
+        git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+        echo "installed rbenv-gem-rehash"
+        git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+        echo "installed rbenv-default-gems"
+        echo "bundler\nrails\ncapistrano\npassenger" > "$RBENV_ROOT/default-gems"
+        git clone git://github.com/jf/rbenv-gemset.git ~/.rbenv/plugins/rbenv-gemset
+        echo "installed rbenv-gemset"
+        ;;
+    esac
   * ) 
     echo "rbenv is not installed\n"
     ;;
