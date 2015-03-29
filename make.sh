eb9afb17bdbd2810f1b302fa924d6f60da3a93f3
Won Wook SONG <wsong0512@gmail.com>
2015-03-29 21:01:26 +0900

    fix typo and separate vim plugins

--- make.sh
+++ make.sh
@@ -17,24 +17,7 @@ case $response in
     ;;
 esac
 
+./git.sh
 ./rbenv.sh
-
-read -r -p "Replace current vim settings and plugins? [Y/n]" response
-case $response in
-  [Yy]* )
-    echo "git submodule init"
-    git submodule init
-    echo "git submodule update"
-    git submodule update
-    rm -rf ~/.vimrc
-    rm -rf ~/.vim/
-    cp -R .vim ~/
-    echo "replaced .vim/"
-    cp -R .vimrc ~/
-    echo "replaced .vimrc"
-    ;;
-  * ) 
-    echo "vim settings are not copied\n"
-    ;;
-esac
+./vimplugins.sh
 
---
+++ vimplugins.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+echo "For the following questions, it would be better to answer yes to all of them to apply my settings."
+
+read -r -p "Replace current vim settings and plugins? [Y/n]" response
+case $response in
+  [Yy]* )
+    echo "git submodule init"
+    git submodule init
+    echo "git submodule update"
+    git submodule update
+    rm -rf ~/.vimrc
+    rm -rf ~/.vim/
+    cp -R .vim ~/
+    echo "replaced .vim/"
+    cp -R .vimrc ~/
+    echo "replaced .vimrc"
+    ;;
+  * ) 
+    echo "vim settings are not copied\n"
+    ;;
+esac
+
